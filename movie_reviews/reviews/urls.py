13e1424864b66e61d2d9d1208237a33be8b5015f
ALNA-PM <136195425+ALNA-PM@users.noreply.github.com>
2025-01-22 15:52:35 +0530

    add

--- movie_reviews/reviews/urls.py
+++ movie_reviews/reviews/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('search/', views.search_movie, name='search_movie'),
-    path('movie/<int:pk>/', views.movie_detail, name='movie_detail'),
+    path('movie/<int:movie_id>/', views.movie_detail, name='movie_detail'),
     path('movie/<int:movie_id>/review/', views.add_review, name='add_review'),
     path('test-mongo/', views.test_mongo_connection, name='test_mongo_connection'),
 ]
